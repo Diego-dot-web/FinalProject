57ebb7bee964fa543739b63c07c76c777c0b6174
Diego <134121369+Diego-dot-web@users.noreply.github.com>
2024-01-11 14:05:03 -0500

    Finish Categories in the main page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,5 +8,6 @@ export default {
 		require('tailwindcss/plugin')(({ addVariant }) => {
 			addVariant('search-cancel', '&::-webkit-search-cancel-button');
 		  }),
+		require('@shrutibalasa/tailwind-grid-auto-fit'),
 	],
 }
